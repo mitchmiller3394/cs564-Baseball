ae693b04d86d0e8fd8a8a0fa080e5e2d44c4f0d5
mjmille4 <mitchmiller3394@yahoo.com>
2025-05-24 19:49:15 -0500

    create home page and navbar

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
-const mysql = require('mysql2');
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const methodOverrid = require('method-override');
+//const session = require('express-session');
+//const flash = require('connect-flash');
+//const userRoutes = require('./routes/userRoutes');
+//const meRoutes = require('./routes/meRoutes');
+//const passport = require('passport');
+//const LocalStrategy = require('passport-local');
 
-const connection = mysql.createConnection({
+// MySQL connection and Pool creation
+const mysql = require('mysql2');
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'Mmeyes3394!',
     database: 'cs564',
 });
 
-connection.connect(err => {
+// Connection and query example
+const sql = "SELECT name FROM students WHERE idstudents = 1";
+pool.getConnection((err, conn) => {
     if (err) {
-        console.error('Error connecting:', err.stack);
+        console.error('Error getting connection:', err.stack);
         return;
     }
-    console.log('Connected as id', connection.threadId);
+    conn.query(sql, (err, result) => {
+        conn.release(); // Always release the connection back to the pool
+        if (err) {
+            console.error('Error executing query:', err.stack);
+            return;
+        }
+        if (result.length > 0) {
+            console.log('ID 1 is:', result[0].name);
+        } else {
+            console.log('No student found with id 1');
+        }
+    });
 });
 
-const sql = "INSERT INTO students (name) VALUES ('Mitch Miller')";
-connection.query(sql, (err, result) => {
-    if (err) {
-        console.error('Error executing query:', err.stack);
-        return;
-    }
-    console.log('Inserted row with ID:', result.insertId);
-});
+// Express starting and views with ejs
+const app = express();
 
-setTimeout(() => {
-    console.log('Waited 5 seconds');
-    connection.end();
-}, 5000);
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+console.log(app.get('env'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverrid('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on Port ${port}`)
+})
