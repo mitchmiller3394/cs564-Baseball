dd534fbc8613d64af58d52f2a7a21c002862bbb2
Will Brodowski <wbrodow@gmail.com>
2025-08-13 18:23:13 -0500

    Added ability to find top first names by position

--- controllers/randomControllers.js
+++ controllers/randomControllers.js
@@ -12,7 +12,7 @@ module.exports.renderEntry = (req, res) => {
 }
 
 module.exports.getRandomStat = async (req, res) => {
-    const { randomStat, player1_first, player1_last, player2_first, player2_last, first_name } = req.body;
+    const { randomStat, player1_first, player1_last, player2_first, player2_last, first_name, position } = req.body;
     try {
         console.log('Received POST:', req.body);
         if (randomStat === 'compareTwoPlayerDropoff') {
@@ -28,6 +28,14 @@ module.exports.getRandomStat = async (req, res) => {
             const [results] = await pool.promise().query(
                 'CALL GetTopPaidPlayerByFirstName(?)',
                 [first_name]
+            );
+            console.log('Stored procedure results:', results);
+            return res.render('randoms/result', { results: results[0] || results });
+        } else if (randomStat === 'getTopFirstNamesByPosition') {
+            console.log('Calling stored procedure GetTopFirstNamesByPosition...');
+            const [results] = await pool.promise().query(
+                'CALL GetTopFirstNamesByPosition(?)',
+                [position]
             );
             console.log('Stored procedure results:', results);
             return res.render('randoms/result', { results: results[0] || results });
